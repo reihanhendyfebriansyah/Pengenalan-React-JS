21b056d66db4cc013d0b90e43ca517f9a8ad5389
reihanhendyfebriansyah <70526707+reihanhendyfebriansyah@users.noreply.github.com>
2020-09-10 08:30:39 +0700

    Add files via upload

---
+++ Alert.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';  
+class Alert extends Component {  
+  render(){  
+    return (  
+      <div className="alert alert-sucess"> 
+      <center>
+        <h4>React JS itu</h4>  
+        seru pakek banget
+        </center>  
+      </div>  
+    );  
+  }  
+}  
+export default Alert;  
